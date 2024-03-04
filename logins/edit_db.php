acc7528c086f9aeb9dd9f97302a4367a2a7d789e
kitmint <154305160+kitmint@users.noreply.github.com>
2024-03-04 14:50:17 +0700

    Update edit_db.php

--- logins/edit_db.php
+++ logins/edit_db.php
@@ -1,29 +1,34 @@
 <?php 
-    session_start();
-    require_once '../config/db.php';
+session_start();
+require_once '../config/db.php';
 
+if (isset($_POST['updateBtn'])) {
+    $user_id = $_POST['userid'];
+    $firstname = $_POST['firstname'];
+    $lastname = $_POST['lastname'];
+    $phonenumber = $_POST['phonenumber'];
 
-    if(isset($_POST['updateBtn'])){
-        $user_id = $_POST['userid'];
-        $firstname = $_POST['firstname'];
-        $lastname = $_POST['lastname'];
-        // $email = $_POST['email'];
+    $query = $conn->prepare("UPDATE users SET firstname = :firstname, lastname = :lastname, phonenumber = :phonenumber WHERE id = :id");
+    $query->bindParam(':firstname', $firstname);
+    $query->bindParam(':lastname', $lastname);
+    $query->bindParam(':id', $user_id);
+    $query->bindParam(':phonenumber', $phonenumber);
+    $query->execute();
 
-        $query = $conn->prepare("UPDATE users SET firstname = :firstname, lastname = :lastname, email = :email WHERE id = :id");
-        $query->bindParam(':firstname', $firstname);
-        $query->bindParam(':lastname', $lastname);
-        // $query->bindParam(':email', $email);
-        $query->bindParam(':id', $user_id);
-        $query->execute();
+    
 
-        if($query){
-            header("location: edit.php");
-        }else{
-            echo "Something went wrong please try again!";
-        }
+    if ($query) {
+        $_SESSION['update_message'] = 'Update successful!';
+    } else {
+        $_SESSION['update_message'] = 'Update failed. Please try again!';
     }
 
-    if(isset($_POST['homepage'])){
-        header("location: user.php");
-    }
+    header("location: edit.php");
+    exit();
+}
+
+if (isset($_POST['myprofile'])) {
+    header("location: userprofile.php");
+    exit();
+}
 ?>
